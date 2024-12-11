12bceda45ca006dcc84e08e0dce02a37a59313b6
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2024-12-11 14:10:12 +0530

    cors + frontend with axios and zustand

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import messageRoutes from './routes/message.route.js';
 import dotenv from 'dotenv';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 dotenv.config()
 const app = express();
@@ -12,6 +13,13 @@ const PORT = process.env.PORT;
 
 app.use(express.json()); // Extract the json data from the body, when we do req.body in controller
 app.use(cookieParser()); // It's allow you to parse the cookies, in protectRoute.js
+
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+    })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
