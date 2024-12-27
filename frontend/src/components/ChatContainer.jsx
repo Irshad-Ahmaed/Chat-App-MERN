59efe0596e629be12cb07ae37e85e97aaba079e6
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2024-12-27 18:00:33 +0530

    ChatContainer + SkeletonLoad + VhatHeader

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,28 @@
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import ChatHeader from "./ChatHeader";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
 
 
 const ChatContainer = () => {
+  const {messages, getMessage, isMessagesLoading, selectedUser} = useChatStore();
+
+  useEffect(()=> {
+    getMessage(selectedUser._id)
+  }, [selectedUser._id, getMessage]);
+
+  if(!isMessagesLoading) return(
+    <div className="flex flex-1 flex-col overflow-auto">
+      <ChatHeader/>
+      <MessageSkeleton/>
+    </div>
+  )
+
   return (
-    <div>ChatContainer</div>
+    <div className="flex flex-1 flex-col overflow-auto">
+      <ChatHeader/>
+      <p>messages...</p>
+    </div>
   )
 }
 
