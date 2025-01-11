580e15e35d6787ba5777b25f45eb0ee5257e84ca
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2025-01-11 21:15:07 +0530

    Chat container UI fixes

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -40,9 +40,9 @@ const ChatContainer = () => {
   )
 
   return (
-    <div className="flex flex-1 flex-col overflow-auto">
+    <div className="flex h-full flex-1 flex-col overflow-auto">
       <ChatHeader/>
-      <div className="flex-1 max-h-[28rem] lg:max-h-[40rem] overflow-y-auto p-4 space-y-4">
+      <div className="flex-1 max-h-[30rem] md:max-h-[35rem] lg:max-h-[40rem] overflow-y-auto p-4 space-y-4">
         {messages.map((message) => (
           <div
             key={message._id}
