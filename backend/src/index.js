6c2cf568c8c440cf4c435ab0c38c88d6eed6fa6f
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2025-01-27 20:49:41 +0530

    cors fixing

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,18 +23,14 @@ webPush.setVapidDetails(
     VAPID_PRIVATE_KEY
 );
 
-
-// const app = express(); // Removed because created on socket.js
-
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
 
 app.use(express.json()); // Extract the json data from the body, when we do req.body in controller
 app.use(cookieParser()); // It's allow you to parse the cookies, in protectRoute.js
 
 app.use(
     cors({
-        origin: ['http://localhost:5173', 'https://chat-app-mern-dtjf.vercel.app/'],
+        origin: 'https://chat-app-mern-dtjf.vercel.app/',
         credentials: true,
     })
 );
@@ -42,14 +38,6 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/notification", notificationRoutes);
 app.use("/api/message", messageRoutes);
-
-// if(process.env.NODE_ENV === "production"){
-//     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-//     app.get("*", (req, res)=> {
-//         res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-//     })
-// }
 
 server.listen(PORT, ()=> {
     console.log("Server is running on port:" + PORT);
