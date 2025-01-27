5c3eed936ece105f8bddc3553bd4a9e1e03e7371
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2025-01-27 20:55:19 +0530

    cors fixing

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const prod = 'https://chat-app-mern-lac.vercel.app'
 const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.MODE === "development" ? "http://localhost:5000" : prod}/api`,
+    baseURL: `${prod}/api`,
     withCredentials: true, // get the access of cookies and etc. from frontend
 });
 
