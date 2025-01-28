5b14eae66930e944a74f8ef702dfae5df8f072a3
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2025-01-28 10:47:24 +0530

    added be url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://chat-app-mern-coral.vercel.app/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://chat-app-mern-kappa.vercel.app/api",
     withCredentials: true, // get the access of cookies and etc. from frontend
 });
 
