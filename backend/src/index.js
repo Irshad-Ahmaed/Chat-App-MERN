024213d187ef5b4b266f32e74f40505cfd728c94
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2025-01-28 12:04:53 +0530

    fe

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,7 +34,7 @@ app.use(cookieParser()); // It's allow you to parse the cookies, in protectRoute
 
 app.use(
     cors({
-        origin: ['https://chat-app-mern-henna-nu.vercel.app', 'http://localhost:5173'],
+        origin: ['http://localhost:5173', 'https://chat-app-mern-henna-nu.vercel.app'],
         credentials: true,
     })
 );
